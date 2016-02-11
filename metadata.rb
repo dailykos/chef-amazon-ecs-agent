7156f9d3cd22cda267dd733bbe5f92b135af47a2
Bram Swenson <bram@craniumisajar.com>
2016-02-11 10:43:25 -0500

    v1.1.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'williamejsalt@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures amazon-ecs-agent'
 long_description 'Installs/Configures amazon-ecs-agent'
-version '1.1.4'
+version '1.1.5'
 
 supports 'ubuntu'
 
